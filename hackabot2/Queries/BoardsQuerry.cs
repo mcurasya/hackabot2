8f1fcc6fcca9a97fa3485e969d76279183c9b6a2
BananchickPasha <smidy999green@gmail.com>
2019-10-26 19:25:05 +0300

    one command may be ready

--- hackabot2/Queries/BoardsQuerry.cs
+++ hackabot2/Queries/BoardsQuerry.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using hackabot.Db.Model;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -15,6 +16,37 @@ namespace hackabot.Queries
             return new QuerryResponse().EditMessageMarkup(account, message.Message.MessageId, BoardsKeyboard(boards));
         }
         //todo askold
-        public static InlineKeyboardMarkup BoardsKeyboard(IEnumerable<Board> boards) => new InlineKeyboardMarkup(boards.Select(t => t.Name));
+        public static InlineKeyboardMarkup BoardsKeyboard(IEnumerable<Board> boards)
+        {
+            var input = boards.ToArray();
+            var keys = new List<List<InlineKeyboardButton>>();
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                var board = input[i];
+                var button =
+                    new InlineKeyboardButton()
+                    {
+                        Text = board.Name,
+                        CallbackData = PackParams("get_board", board.Name, board.Name)
+                    };
+                if (keys.Count == 0)
+                {
+                    keys.Add(new List<InlineKeyboardButton> { button });
+                }
+                else if (keys.Count > 0)
+                {
+                    if (keys.Last().Count == 1)
+                    {
+                        keys.Last().Add(button);
+                    }
+                    else
+                    {
+                        keys.Add(new List<InlineKeyboardButton> { button });
+                    }
+                }
+            }
+            return keys.ToArray();
+        }
     }
 }
