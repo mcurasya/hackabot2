ed8d13fab8618bacc906f2cc34e887907db704e3
yura <mcurasya@gmail.com>
2019-10-26 15:47:28 +0300

    moar enums

--- hackabot2/Db/TelegramContext.cs
+++ hackabot2/Db/TelegramContext.cs
@@ -5,7 +5,7 @@ namespace hackabot
     public class TelegramContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=database.db");
