e6fe7d34f2cbd938cddd25bb6d0e2236b0efec7a
yura <mcurasya@gmail.com>
2019-10-26 14:36:21 +0300

    initial commit

---
+++ hackabot2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackabot2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
