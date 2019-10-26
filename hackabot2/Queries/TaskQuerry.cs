f796eca9ba2f5ec21bba88144b6fd73a0b365501
BananchickPasha <smidy999green@gmail.com>
2019-10-26 21:35:12 +0300

    awful taskquerry

--- hackabot2/Queries/TaskQuerry.cs
+++ hackabot2/Queries/TaskQuerry.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using hackabot.Db.Model;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
+
 
 namespace hackabot.Queries
 {
@@ -14,39 +16,11 @@ namespace hackabot.Queries
             var board = account.Controller.GetBoards(account).First(b => b.Id.ToString() == values.First().Key);
             var taskId = values.First().Value;
             var task = account.Controller.GetTasks(board, account);
+            return null;
         }
         public static InlineKeyboardMarkup TaskButton(Task task)
         {
-
-            var input = task.Name;
-            var keys = new List<List<InlineKeyboardButton>>();
-            
-            for (int i = 0; i < input.Length; i++)
-            {
-                var task = input[i];
-                var button =
-                    new InlineKeyboardButton()
-                    {
-                        Text = task.Name,
-                        CallbackData = PackParams("get_task", board.Id.ToString(), task.Id.ToString())
-                    };
-                if (keys.Count == 0)
-                {
-                    keys.Add(new List<InlineKeyboardButton> { button });
-                }
-                else if (keys.Count > 0)
-                {
-                    if (keys.Last().Count == 1)
-                    {
-                        keys.Last().Add(button);
-                    }
-                    else
-                    {
-                        keys.Add(new List<InlineKeyboardButton> { button });
-                    }
-                }
-            }
-            return keys.ToArray();
+            throw new NotImplementedException();
         }
     }
 }
